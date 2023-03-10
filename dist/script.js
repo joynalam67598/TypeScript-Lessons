88890206b6e4262f8940a5dcc0f2acdc70f9c5ef
joynal_alam <joynal.alam@enosisbd.com>
2023-03-11 01:34:13 +0600

    generices, generices in interface in typescript

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,10 @@
-export {};
+"use strict";
+const response = {
+    status: 200,
+    type: "OK",
+    data: {
+        name: "name",
+        age: 400,
+    },
+};
 //# sourceMappingURL=script.js.map
